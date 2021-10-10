8d1f296f0ed15140b76137eb9803d71f6fd6a5f3
jasonschreib <jwschreiber1@gmail.com>
2021-10-10 17:16:29 -0400

    Implemented pandas scavenger_hunt function

--- HW2.py
+++ HW2.py
@@ -6,9 +6,27 @@ from collections import defaultdict
 def scavenger_hunt() -> dict:
     #read the file into a dataframe
     df = pd.read_csv('wine.csv')
-    #display the df
-    print(df.shape)
+    #calculate dimensions of the dataframe
+    answer0 = df.shape #(178, 15)
+    #calculate avg alcohol content over all wines
+    answer1 = df['alcohol'].mean()
+    #calculate the st. dev. of magnesium content
+    answer2 = df['magnesium'].std()
+    #mean alcohol content grouped by target and displayed as a list
+    answer3 = df.groupby(['target']).mean()['alcohol'].tolist()
+    #minimum proline content and maximum proline content
+    answer4 = [(df['proline'].min()), (df['proline'].max())]
+    #identify # rows in dataset have malic_acid content > 2.5
+    answer5 = len(df[df['malic_acid'] > 2.5])
+    #identify index of wine with lowest flavanoid content
+    answer6 = df['flavanoids'].idxmin()
+    #identify num of unique hue vals in dataset
+    answer7 = len(df['hue'].unique())
 
+    #all answers to scavenger hunt in dictionary
+    answers = {0: answer0, 1: answer1, 2: answer2, 3: answer3, 4: answer4, 5: answer5, 6: answer6, 7: answer7}
+    #return dictionary
+    return answers
 
 def get_words(file_path) -> list:
     pass
